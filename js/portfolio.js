031c94ad966d696eb471642884ac9fa385ec7871
Ryan Dsouza <ryandsz2347@gmail.com>
2020-12-29 14:00:12 +0000

    Added Timeline

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,6 +8,31 @@ $( document ).ready(function() {
 	});
 
 	$('.carousel').carousel({
-	        interval: 3000,
+	    interval: 3000,
 	});
+
+	const scrollToTopButton = document.getElementById('js-top');
+
+	const scrollFunc = () => {
+	  let y = window.scrollY;
+	  if (y > 0) {
+	    $('.top-link').show();
+	  } else {
+	    $('.top-link').hide();
+	  }
+	};
+	
+	window.addEventListener("scroll", scrollFunc);
+
+	const scrollToTop = () => {
+	  const c = document.documentElement.scrollTop || document.body.scrollTop;
+	  if (c > 0) {
+	    window.scrollTo({top: 0, behavior: 'smooth'});
+	  }
+	};
+
+	scrollToTopButton.onclick = function(e) {
+	  e.preventDefault();
+	  scrollToTop();
+	}
 });
